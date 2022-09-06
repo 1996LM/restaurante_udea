7bace65fbb0f34a160b671986d2ec95cfd320d62
1996LM <lmiguel.hoyos@gmail.com>
2022-09-06 11:16:32 -0500

    creacion de base de datos para el cliente

--- main.py
+++ main.py
@@ -3,23 +3,28 @@ from argparse import ArgumentParser
 from restaurant_sistem.database_service.customers import CustomersDataBase
 
 def execute(args):
-    customers_database = CustomersDataBase(database_name=args.database_name)
-
+    customers_database = CustomersDataBase(database_name = args.database_name)
+    
     while True:
         # nombre, edad, telefono, numero_factura
-        user_input = input("ingrese los datos del cliente:")
-        name, age, phone_number, invoice_number = user_input.split(" ")
+        user_input = input("Ingrese los datos del cliente:")
+        name, age, phone_number, invoice_number = user_input.split("")
 
-        print(name)
-        print(age)
-        print(phone_number)
-        print(invoice_number)
+        customers_id = customers_database.create({
+            "name": name,
+            "age": age,
+            "phone_number": phone_number,
+            "invoice_number": invoice_number
+        })
+
+        print(customers_database.read(customers_id)) #verificar que el registro se creo bien
+
 
 def main():
     parser = ArgumentParser()
 
     parser.add_argument(
-        "-dn", "--database.name", type = str, 
+        "-dn", "--database-name", type = str, 
         choices = ["sedes", "empleados","clientes", "proveedores", "productos", "menus"]
     )
     args = parser.parse_args()
@@ -29,5 +34,6 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
